e17406917a1ed25fc540f23d60c7e105adb8f801
Alen Morillo Meneses <alenmisaelmorillomeneses@gmail.com>
2024-09-28 17:47:35 -0300

    Se mejoro la logica del login

--- sources/sec/sec/views.py
+++ sources/sec/sec/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
+def staff_required(view_func):
+    @login_required
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_staff:
+            return view_func(request, *args, **kwargs)
+        else:
+            return redirect(reverse('login'))  
+    return _wrapped_view
+
+
+@staff_required
 def administrativo(request):
 #    print (request.user.persona)
     return render(request,"administrativo.html")
