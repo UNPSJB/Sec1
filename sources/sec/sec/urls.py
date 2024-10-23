6606187843e17ea4a704e5b43c43d2305f6b8331
JotaCe08 <joacocastro1999@gmail.com>
2024-10-23 02:37:22 -0300

    cambios templates y otras cositas

--- sources/sec/sec/urls.py
+++ sources/sec/sec/urls.py
@@ -25,23 +25,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('administrativo/', administrativo, name= 'administrativo'),
-    path('contacto/', contacto, name= 'contacto'),
     path('', home, name= 'home'),
     path('personas/', include('apps.personas.urls')),
-    # path('listadoSalones/', listadoSalones, name='listadoSalones'),
-    # path('listadoAlumnos/', listadoAlumnos, name='listadoAlumnos'),
-    # path('listadoAlquileres/', listadoAlquileres, name='listadoAlquileres'),
-    # path('listadoCursos/', include('apps.cursos.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login'),  # Usamos la vista genérica
     path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
-    path('usuario/', usuario),
     path('admin/', admin.site.urls),
-    path('registro/', registro, name= 'registro'),
-    # path('verAlumno/', verAlumno, name='verAlumno'),
-    # path('verAlquiler/', verAlquiler, name='verAlquiler'),
-    # path('verProfesor/', verProfesor, name='verProfesor'),
-    # path('verCurso/', verCurso, name='verCurso'),
-    # path('verSalon/', verSalon, name='verSalon'),
     path('salones/', include('apps.salones.urls')),
     path('afiliados/', include('apps.afiliados.urls')),
     path('cursos/', include('apps.cursos.urls')),
--- sources/sec/sec/views.py
+++ sources/sec/sec/views.py
@@ -16,50 +16,10 @@ def staff_required(view_func):
 
 @staff_required
 def administrativo(request):
-#    print (request.user.persona)
     return render(request,"administrativo.html")
-
-
-def contacto(request):
-    return render(request,"contacto.html")
-
 
 def home(request):
     return redirect("login")
 
-# def listadoCursos(request):
-#     return render(request,"listadoCursos.html")
-
-# def listadoSalones(request):
-#     return render(request,"listadoSalones.html")
-
 def login(request):
     return render(request,"login.html")
-
-
-def usuario(request):
-    return render(request,"usuario.html")
-
-def registro(request):
-    return render(request, "registro.html")
-
-# def listadoAlquileres(request):
-#     return render(request,"listadoAlquileres.html")
-
-# def listadoAlumnos(request):
-#     return render(request,"listadoAlumnos.html")
-
-# def verAlumno(request):
-#     return render(request,"verAlumno.html")
-
-# def verAlquiler(request):
-#     return render(request,"verAlquiler.html")
-
-# def verProfesor(request):
-#     return render(request,"verProfesor.html")
-
-# def verCurso(request):
-#     return render(request,"verCurso.html")
-
-# def verSalon(request):
-#     return render(request,"verSalon.html")
